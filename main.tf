022ff75768ff8550ecec2db764b08ab946fa5577
opornik2 <opornik@gmail.com>
2026-06-05 12:23:24 +0200

    changed region and zone

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 ### PROVIDER
 provider "google" {
-  project = "advancedterraform" #replace this with your project-id
-  region  = "us-central1"
-  zone    = "us-central1-a"
+  project = "project-42f18089-ccf7-4574-a47" #replace this with your project-id
+  region  = "europe-central2"
+  zone    = "europe-central2-a"
 }
 
 ### NETWORK
@@ -15,7 +15,7 @@ resource "google_compute_subnetwork" "subnet-1" {
   name                     = "subnet1"
   ip_cidr_range            = "10.127.0.0/20"
   network                  = data.google_compute_network.default.self_link
-  region                   = "us-central1"
+  region                   = "europe-central2"
   private_ip_google_access = true
 }
 
